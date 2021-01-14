eeeb67da34c626a1ab010c0cd9ea7b22920ca0dc
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-01-14 15:14:25 +0100

    observer and subject interface

---
+++ src/com/nubari/Observer.java
@@ -0,0 +1,5 @@
+package com.nubari;
+
+public interface Observer {
+    public void update(String test);
+}
---
+++ src/com/nubari/Subject.java
@@ -0,0 +1,7 @@
+package com.nubari;
+
+public interface Subject {
+    public void registerObserver(Observer observer);
+    public void removeObserver(Observer observer);
+    public void notifyObservers();
+}
